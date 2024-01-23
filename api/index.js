f50ace85dcf98c103099a60718a16bf30b6a8b49
Vira <virko1996@gmail.com>
2024-01-23 08:01:14 +0200

    vercel/api

--- api/index.js
+++ api/index.js
@@ -66,12 +66,12 @@ const getUserDataFromReq = (req) => {
   });
 };
 
-app.get('/test', (req, res) => {
+app.get('/api/test', (req, res) => {
   mongoose.connect(process.env.MONGO_URL); // connect to data base
   res.json('test ok');
 });
 
-app.post('/register', async (req, res) => {
+app.post('/api/register', async (req, res) => {
   mongoose.connect(process.env.MONGO_URL); // connect to data base
   const { name, email, password } = req.body; // from this data we create a User
   try {
@@ -86,7 +86,7 @@ app.post('/register', async (req, res) => {
   }
 });
 
-app.post('/login', async (req, res) => {
+app.post('/api/login', async (req, res) => {
   mongoose.connect(process.env.MONGO_URL); // connect to data base
   const { email, password } = req.body;
   const user = await User.findOne({ email });
@@ -113,7 +113,7 @@ app.post('/login', async (req, res) => {
   }
 });
 
-app.get('/profile', (req, res) => {
+app.get('/api/profile', (req, res) => {
   mongoose.connect(process.env.MONGO_URL); // connect to data base
   const { token } = req.cookies;
   if (token) {
@@ -127,18 +127,22 @@ app.get('/profile', (req, res) => {
   }
 });
 
-app.post('/logout', (req, res) => {
+app.post('/api/logout', (req, res) => {
   res.cookie('token', '').json(true);
 });
 
-app.post('/upload-by-link', async (req, res) => {
+app.post('/api/upload-by-link', async (req, res) => {
   const { link } = req.body;
   const newName = 'photo' + Date.now() + '.jpg';
   await imageDownloader.image({
     url: link,
     dest: '/tmp/' + newName,
   });
-  const url = await uploadToS3('/tmp/' + newName, newName, mime.lookup('/tmp/' + newName));
+  const url = await uploadToS3(
+    '/tmp/' + newName,
+    newName,
+    mime.lookup('/tmp/' + newName)
+  );
   res.json(url);
 });
 
@@ -153,7 +157,7 @@ app.post('/upload', photosMiddleware.array('photos', 100), async (req, res) => {
   res.json(uploadedFiles);
 });
 
-app.post('/places', (req, res) => {
+app.post('/api/places', (req, res) => {
   mongoose.connect(process.env.MONGO_URL); // connect to data base
   const { token } = req.cookies;
   const {
@@ -187,7 +191,7 @@ app.post('/places', (req, res) => {
   });
 });
 
-app.get('/user-places', (req, res) => {
+app.get('/api/user-places', (req, res) => {
   mongoose.connect(process.env.MONGO_URL); // connect to data base
   const { token } = req.cookies;
   jwt.verify(token, jwtSecret, {}, async (err, userData) => {
@@ -196,13 +200,13 @@ app.get('/user-places', (req, res) => {
   });
 });
 
-app.get('/places/:id', async (req, res) => {
+app.get('/api/places/:id', async (req, res) => {
   mongoose.connect(process.env.MONGO_URL); // connect to data base
   const { id } = req.params;
   res.json(await Place.findById(id));
 });
 
-app.put('/places', async (req, res) => {
+app.put('/api/places', async (req, res) => {
   mongoose.connect(process.env.MONGO_URL); // connect to data base
   const { token } = req.cookies;
   const {
@@ -240,12 +244,12 @@ app.put('/places', async (req, res) => {
   });
 });
 
-app.get('/places', async (req, res) => {
+app.get('/api/places', async (req, res) => {
   mongoose.connect(process.env.MONGO_URL); // connect to data base
   res.json(await Place.find());
 });
 
-app.post('/bookings', async (req, res) => {
+app.post('/api/bookings', async (req, res) => {
   mongoose.connect(process.env.MONGO_URL); // connect to data base
   const userData = await getUserDataFromReq(req);
   const { place, checkIn, checkOut, numberOfGuests, name, phone, price } =
@@ -268,7 +272,7 @@ app.post('/bookings', async (req, res) => {
     });
 });
 
-app.get('/bookings', async (req, res) => {
+app.get('/api/bookings', async (req, res) => {
   mongoose.connect(process.env.MONGO_URL); // connect to data base
   const userData = await getUserDataFromReq(req);
   res.json(await Booking.find({ user: userData.id }).populate('place'));
